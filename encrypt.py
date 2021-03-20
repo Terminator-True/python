75b52f5ceceebb40f7e6be5d460c1b5749a680a8
Terminator-True <jf28renault@gmail.com>
2021-03-21 00:27:30 +0100

    024

---
+++ encrypt.py
@@ -0,0 +1,27 @@
+"""
+Encriptar cosa :3
+"""
+import random
+abc="abcdefghijklmnñopqrstuvwxyz"
+abc2="АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШ"
+imprimir=""
+codify=False
+while codify != True:
+    User_input=int(input("Elegir(1-Encript 2-Desencript): "))
+    if User_input==1:
+        Nombre=input("Nombre: ")
+        for i in range(len(Nombre)):
+            for j in range(len(abc)):
+                if Nombre[i]==abc[j]:
+                    imprimir+=abc2[j]
+        print(imprimir)
+
+    if User_input==2:
+        Nombre=input("Nombre: ")
+        for i in range(len(Nombre)):
+            for j in range(len(abc2)):
+                if Nombre[i]==abc2[j]:
+                    imprimir+=abc[j]
+        print(imprimir)
+    else:
+        break
