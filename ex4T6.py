f00987a9273e2c2d53c58e4c8126fe2372eba159
Terminator-True <jf28renault@gmail.com>
2021-03-20 11:34:34 +0100

    023

---
+++ ex4T6.py
@@ -0,0 +1,11 @@
+"""
+Fes un programa en Python anomenat ex4.py que comprovi que el nombre d’arguments és superior a 1,
+ que són alfabètics i en aquest cas que els ordeni alfabèticament. Sinó que mostri quin error hi ha.
+
+"""
+import sys
+
+if len(sys.argv)>1:
+
+else:
+    print("Error, la llista ha de ser major a 1")
--- llistes.py
+++ llistes.py
@@ -1,7 +1,12 @@
 import random
-def inicialitza (w,l,m,M):
-     w=[random.randint(m,M) for i in range(l)]
-
+def omple_llista (w,l,m,M):
+     return [random.randint(m,M) for i in range(l)]
+      
 def demana_llista(n):
-    return [input("Afegeix algun carácter: ") for i in range(n)] 
-    
+    return [int(input("Afegeix algun carácter: ")) for i in range(n) ] 
+def imprimeix_llista(ll):
+     print(ll)
+def ordena_llista(ll):
+     return ll.sort()
+def suma_llistes(ll1,ll2):
+     return [ll1[i]+ll2[i] for i in range(len(ll1))]
