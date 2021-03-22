c821d1199a0dbd7b3ac8a1830ec04be8f188214a
Terminator-True <jf28renault@gmail.com>
2021-03-22 12:41:20 +0100

    025

--- Proves.py
+++ Proves.py
@@ -1,27 +1,6 @@
 from tkinter import *
 root = Tk()
 
-def Take_input(): 
-    INPUT = inputtxt.get("1.0", "end-1c") 
-    print(INPUT) 
-    if(INPUT == "120"): 
-        Output.insert(END, 'Correct') 
-    else: 
-        Output.insert(END, "Wrong answer") 
-
-l = Label(text = "What is 24 * 5 ? ") 
-inputtxt = Text(root, height = 10, 
-                width = 25, 
-                bg = "light yellow") 
-
-Output = Text(root, height = 5,
-              width = 25,
-              bg = "light cyan") 
-
-Display = Button(root, height = 2, 
-                 width = 20,
-                 text ="Show", 
-                 command = lambda:Take_input())
 """
 
 while acabat is not True:
--- encrypt.py
+++ encrypt.py
@@ -2,32 +2,69 @@
 Encriptar cosa :3
 """
 import random
+from tkinter import *
+from tkinter import ttk
+
+
 abc="abcdefghijklmnñopqrstuvwxyz"
 abc2="АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШ"
-imprimir=""
-codify=False
-while codify != True:
-    User_input=int(input("Elegir(1-Encript 2-Desencript): "))
-    if User_input==1:
-        Nombre=input("Nombre: ")
-        for i in range(len(Nombre)):
-            if Nombre[i]==" ":
-                imprimir+=" "
-            else:    
-                for j in range(len(abc)):
-                    if Nombre[i]==abc[j]:
-                        imprimir+=abc2[j]
-        print(imprimir)
+acabat=False
 
-    if User_input==2:
-        Nombre=input("Nombre: ")
-        for i in range(len(Nombre)):
-            if Nombre[i]==" ":
-                imprimir+=" "
-            else:    
-                for j in range(len(abc2)):
-                    if Nombre[i]==abc2[j]:
-                        imprimir+=abc[j]
-        print(imprimir)
-    else:
-        break
+def encriptar(Nombre):
+    imprimir=""
+    for i in range(len(Nombre)):
+        if Nombre[i]==" ":
+            imprimir+=" "
+        else:    
+            for j in range(len(abc)):
+                if Nombre[i]==abc[j]:
+                    imprimir+=abc2[j]
+    return imprimir
+
+
+def desencriptar(Nombre):
+    imprimir=""
+    for i in range(len(Nombre)):
+        if Nombre[i]==" ":
+            imprimir+=" "
+        else:    
+            for j in range(len(abc2)):
+                if Nombre[i]==abc2[j]:
+                    imprimir+=abc[j]
+    return imprimir
+
+def clicked_encrypt():
+    cripto=encriptar(txt1.get())
+    lbl1.configure(text = cripto)
+
+window = Tk()
+
+window.title("Whatsapp Encriptado :3")
+
+window.geometry('300x300')
+
+tab_control = ttk.Notebook(window)
+
+tab1 = ttk.Frame(tab_control)
+
+tab2 = ttk.Frame(tab_control)
+
+tab_control.add(tab1, text='encrypt')
+
+tab_control.add(tab2, text='Desencrypt')
+
+lbl1 = Label(tab1, text= 'label1')
+
+lbl1.grid(column=0, row=0)
+
+lbl2 = Label(tab2, text= 'label2')
+
+lbl2.grid(column=0, row=0)
+
+tab_control.pack(expand=1, fill='both')
+
+txt1 = Entry(window,width=20)
+
+btn1 = Button(window ,text="➤" ,command=clicked_encrypt) 
+
+window.mainloop()
