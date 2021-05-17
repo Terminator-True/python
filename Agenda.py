0b4f3f219d06a9724fe0f1f0444260feb151132b
Terminator-True <jf28renault@gmail.com>
2021-05-17 19:11:13 +0200

    043

---
+++ Agenda.py
@@ -0,0 +1,136 @@
+"""
+Hola, us faig 5 cèntims de com encarar la pràctica.
+1r teniu clar com és l'arxiu de dades:
+per exemple, una línia seria:
+"Pepet, Nosurt, 666777888, pepet@nosurt.com, @pepetnosurt"
+1r feu el menú:
+per exemple implementat de manera senxilla
+i anar fent
+alta és una funció que:
+demana les dades amb inputs
+construeix la línia separada per comes (no oblideu el salt de línia)
+i l’afegeix a l’arxiu
+opcional: si el correu electrònic o l’usuari de telegram ja existeix, es demanarà a l’usuari si desitja sobreescriure el contacte
+
+llistat:
+obrir l’arxiu en mode lectura
+imprimir cada línia
+(una idea seria posar un número davant de cada línia)
+
+modifica:
+utilitzeu llistat per mostrar les dades
+i podeu demanar quina entrada es vol modificar (si heu afegit el número)
+deseu les dades de l’arxiu en una llista i treballeu amb la llista
+ara podeu demanar quin camp es vol modificar
+feu les modificacions i torneu a construir la línia (podeu fer servir els mètodes split que ens converteix un text en una llista indicant el separador
+	donada una linia = "Pepet, Nosurt, 666777888, pepet@nosurt.com, @pepetnosurt"
+
+liniaLista = [“Pepet”, “Nosurt”, “666777888”, “pepet@nosurt.com”, “@pepetnosurt”]
+
+us pot anar bé per modificar el camp que vulgueu
+torneu a transformar la llista en una cadena (ajuntant cada camp separant per comes)
+substituïu a la llista principal amb totes les dades
+deseu la llista sencera a l’arxiu (obriu en modus w i utilitzeu el mètode writelines)
+
+elimina:
+procediu com en modifica, però ara elimineu la línia corresponent de la llista
+
+recuperar segons un camp:
+aquest apartat és per a qui vulgui nota!!!!!!
+
+"""
+llista_informacio=["Nom","Cognom","Numero Telefon","Correu","Usuari Telegram"]
+def comprovaFitxer(llista):
+	with open ("Agenda.txt	","r+") as f:
+		for linia in f:
+			if llista[3] in linia:
+				return "01"
+			if llista[4] in linia:
+				return "02"	
+def informacio():
+	S_N=""
+	while S_N!="S":
+		llista=[input(el+":") for el in llista_informacio]
+		if comprovaFitxer(llista)=="01":
+			print("Correu es igual a un altre de la agenda")
+			Sobrescriure=input("Sobrescriure?")
+			if Sobrescriure=="N":
+				llista[3]=input("Correu: ")
+		if comprovaFitxer(llista)=="02":
+			print("Usuari de telegram es igual a un altre de la agenda")
+			Sobrescriure=input("Sobrescriure?")
+			if Sobrescriure=="N":
+				llista[4]=input("Usuari Telegram: ")
+		S_N=input(str(llista)+"\n"+"Informació correcte?")
+	return llista
+def Alta():
+	with open ("Agenda.txt	","a+") as f:
+		f.writelines(",".join(informacio()))
+		f.write("\n")
+
+def modifica():
+	with open ("Agenda.txt	","r+") as f:
+		lineas=f.readlines()
+		for i in range (len(lineas)):
+			print("\n",i," ",lineas[i])
+		try:
+			linea=int(input("Línea a modificar: "))
+			modificacio=lineas[linea]
+			liniallista=modificacio.split(",")
+			print(liniallista,"\n",str(llista_informacio))
+			camp=input("Quins camp vols modificar?")
+			for i in range(len(llista_informacio)):
+				if camp==llista_informacio[i]:
+					print("Camp a modificar: ",liniallista[i])
+					liniallista[i]=input("Modificació:")
+					print(liniallista)
+					lineas[linea]=",".join(liniallista)
+					print(lineas)
+					with open ("Agenda.txt	","w") as fi:
+						fi.writelines(lineas)
+		except ValueError:
+			print("Ha de ser un número")
+		except IndexError:
+			print("Error, linia no existent")
+def elimina():
+	with open ("Agenda.txt	","r+") as f:
+		lineas=f.readlines()
+		for i in range (len(lineas)):
+			print("\n",i," ",lineas[i])
+	try:
+		linea=int(input("Línea a Esborrar: "))
+		lineas.pop(linea)		
+		with open ("Agenda.txt	","w") as fi:
+			fi.writelines(lineas)
+
+	except ValueError:
+		print("Ha de ser un número")
+	except IndexError:
+		print("Error, linia no existent")
+def Consulta():
+	with open ("Agenda.txt	","r") as f:
+		camp=input("Buscar: ")
+		for el in f:
+			if camp in el:
+				print(el)
+		
+acabat=False
+imprimir=""
+imprimir+="==================================================\n"+"\n░█████╗░░██████╗░███████╗███╗░░██╗██████╗░░█████╗░\n"+"██╔══██╗██╔════╝░██╔════╝████╗░██║██╔══██╗██╔══██╗\n"+"██╔══██║██║░░╚██╗██╔══╝░░██║╚████║██║░░██║██╔══██║\n"+"██║░░██║╚██████╔╝███████╗██║░╚███║██████╔╝██║░░██║\n"+"╚═╝░░╚═╝░╚═════╝░╚══════╝╚═╝░░╚══╝╚═════╝░╚═╝░░╚═╝\n"+"==================================================\n"
+print(imprimir)
+while acabat is not True:
+	try:
+		print("0: Sortir\n"+"1: Alta\n"+"2: Modifica\n"+"3: Elimina\n"+"4: Consulta")
+		User_input=int(input("Opció a elegir: "))
+		if User_input==0:
+			acabat=True
+		elif User_input==1:
+			Alta()
+		elif User_input==2:
+			modifica()
+		elif User_input==3:
+			elimina()
+		elif User_input==4:
+			Consulta()
+	except ValueError():
+		print("Ha de ser un número")		
