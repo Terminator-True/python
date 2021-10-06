9e4ba8947986bc8b5c3477a03f30747f844448ff
terminator-true <jf28renault@gmail.com>
2021-10-06 19:23:45 +0200

    019

--- 2 n/Tasca 4.1/Modules/split.py
+++ 2 n/Tasca 4.1/Modules/split.py
@@ -2,3 +2,7 @@
 El m`odul t ́e com a responsabilitat trencar una imatge blanc i negre en un conjunt d’imatges
 m ́es petites cadascuna de les quals cont ́e  ́unicament un element (una xifra).
 """
+
+from img import null, subimg
+from transf import vtrim,htrim
+
--- 2 n/Tasca 4.1/Modules/transf.py
+++ 2 n/Tasca 4.1/Modules/transf.py
@@ -4,13 +4,71 @@ Es el m`odul que concentra les eines per transformar imatges. En particular per 
 retallar,  ́es a dir per reduir una imatge fins que nom ́es cont ́e els d ́ıgits que la formen.
 """
 
-from PIL import Image
+from PIL import Image as img
 
 from img import subimg
 
 def vtrim(img):
-    for i in range(len(img)): 
-        for j in range(len(img[j])):
-            if img[i][j]==(0,0,0):
-                
+    for i in range(len(img[1])): 
+        wp = 0
+        for j in range(len(img[1][j])):
+            if img[1][i][j]==(0,0,0):
+                ow = 0
+                oh = i-1
+                #Una vegada que es detecta un píxel negre, fa que la vegada que es detecti una línea completament blanca
+                #Es retalli el caracter
+                detectat = True
+            elif img[1][i][j]==(255,255,255):
+                #White pixel o wp, compta els píxels blancs en una línea
+                wp += 1
+                #Si tota la línea es de píxels blancs, vol dir que el caracter ja ha terminat, així que ho retallem
+                if wp == len(img[1][j]) and detectat:
+                    w=len(img[1][j])
+                    h=i
+                    return subimg(img,ow,oh,w,h)
+        if not detectat:
+            return None
 
+def htrim(img):
+    for i in range(len(img[1])): 
+        wp = 0
+    for j in range(len(img[1][j])):
+        if img[1][j][i]==(0,0,0):
+            oh = 0
+            ow = j-1
+            #Una vegada que es detecta un píxel negre, fa que la vegada que es detecti una línea completament blanca
+            #Es retalli el caracter
+            detectat = True
+        elif img[1][j][i]==(255,255,255):
+            #White pixel o wp, compta els píxels blancs en una línea
+            wp += 1
+            #Si tota la línea es de píxels blancs, vol dir que el caracter ja ha terminat, així que ho retallem
+            if wp == len(img[1][j]) and detectat:
+                w=j
+                h=len(img[1])
+                return subimg(img,ow,oh,w,h)
+
+    if not detectat:
+        return None                
+
+def scale(src, h):
+    """
+    Escala la imatge a l'alçada `h` conservant l'aspect ratio
+    """
+    # Compute src and dst sizes
+    src_h = img.get_h(src)
+    src_w = img.get_w(src)
+
+    # Scaling factor
+    fh = float(src_h) / h
+
+    # dst dimension
+    dst_h = h
+    dst_w = int(round(src_w / fh))
+
+    # Mostrejem matriu original
+    sm = img.matrix(src)
+    dst = [ [sm[int(round(fh*h))][int(round(fh*w))] for w in range(dst_w)] 
+            for h in range(dst_h)]             
+
+    return img.img(dst, '1')
