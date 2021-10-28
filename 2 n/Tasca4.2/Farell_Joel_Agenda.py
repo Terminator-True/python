143fb7a89f5b352fc9e2edf112fc763a8b856d6a
Terminator-True <jf28renault@gmail.com>
2021-10-28 11:35:10 +0200

    032

--- 2 n/Tasca4.2/Farell_Joel_Agenda.py
+++ 2 n/Tasca4.2/Farell_Joel_Agenda.py
@@ -1,8 +1,5 @@
 """
 Dissenyeu una aplicació anomenada cognom_nom_agenda.py que permeti entrar dades de clients en una agenda, mostrar-les i enregistrar-les en un arxiu (amb el format que vulgueu). Cal verificar que totes les dades entrades tinguin el format i la longitud correctes.
-
-
-
 Crea una classe Client que contingui:
 
  - les dades per un sol client: nom,cognoms,data_naixement,telefon
@@ -11,8 +8,6 @@ Crea una classe Client que contingui:
 
  - els mètodes d'impressió de dades: un per a cada atribut
 
-
-
 L'aplicació principal ha de mostrar el següent menú:
 
 1: Introdueix dades a l'agenda.
@@ -20,9 +15,7 @@ L'aplicació principal ha de mostrar el següent menú:
 3: Mostra dades de l'agenda actual.
 4: Exporta l'agenda actual a l'arxiu desitjat.
 5: Sortir
-
 """
-
 class client:
     def __init__(self):
         self.nom,self.cognoms,self.data_naixement,self.telefon = "","","","",""
@@ -47,20 +40,15 @@ class client:
         self.dades=dict(zip(["nom","cognoms","data_naixement","telefon"],[self.mostra_nom(self),self.mostra_cognoms(self),self.mostra_data_naixement(self),self.mostra_telefon(self)]))
     def mostra_dades(self):
         return " ,".join(['{0} : {1}'.format(key,self.dades[key]) for key in self.dades])
-    def __str__(self) -> str:
-        return " ,".join(['{0} : {1}'.format(key,self.dades[key]) for key in self.dades])
-
 def inputValors():
     n = input("Nom:")
     c = input("Cognoms:")
     d = input("data naixement:")
     t = input("teléfon:")
     return n,c,d,t
-
 def ComprovaArchiu(Arxiu):
     try:
         with open (Arxiu,"r") as f:
-            print("Correcte")
             return True
     except FileNotFoundError:
         return False
@@ -75,13 +63,12 @@ def programaPrincipal(User_input,agenda):
         dades_client.definir_data_naixement(dades_client,valors[2])
         dades_client.definir_telefon(dades_client,valors[3])
         dades_client.afegeix_dades(dades_client)
-        print(dades_client)
         agenda.append(dades_client.mostra_dades(dades_client)+"\n")
     elif User_input==2:
         Arxiu = input("Ruta del archiu: ")
         if ComprovaArchiu(Arxiu):
             with open (Arxiu,"r") as f:
-                agenda=f.readlines()
+                agenda.extend(f.readlines())
     elif User_input==3:
         print("\n","".join(agenda))
     elif User_input==4:
