31476704b663a00221db06efd9396cdbe5c4523c
Terminator-True <jf28renault@gmail.com>
2021-11-29 16:42:12 +0100

    039

--- 2 n/MP02/mis_flask_app/hola.py
+++ 2 n/MP02/mis_flask_app/hola.py
@@ -1,6 +1,10 @@
 from flask import Flask,render_template
+from pymongo import MongoClient
 app = Flask(__name__)
 @app.route('/')
 @app.route('/<name>')
 def hello(name=None):
-    return render_template('hello.html', list=[1,2,3,4,5,6,7,8])
+    with MongoClient('localhost',27017,username='admin',password='Admin@123',authSource='JoelFarell') as client:
+        mydb = client.JoelFarell
+        mycol = mydb.estudiants
+    return render_template('hello.html', list=mycol.find())
