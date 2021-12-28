b925d8d834d3917a45fb84675cf311160a935ec4
Terminator-True <jf28renault@gmail.com>
2021-12-28 13:59:56 +0100

    046

--- 2n/MP03/Pojecte-UF1/Projecte.py
+++ 2n/MP03/Pojecte-UF1/Projecte.py
@@ -1,10 +1,10 @@
 import os 
-
+directori = "/home/joel/Escritorio/python/2n/MP03/Pojecte-UF1"
 class album:
     def __init__(self,ruta,cançons,genere,any,autor,numero_cops):
         self.ruta,self.cançons,self.genere,self.any,self.autor,self.numero_cops=ruta,cançons,genere,any,autor,numero_cops
     def mostra(self):
-        return self.ruta,self.cançons,self.genere,self.any,self.autor,self.numero_cops
+        return ",".join([self.ruta,"|".join(self.cançons),self.genere,self.any,self.autor,str(self.numero_cops)])
 
 def ComprovaArchiu(Arxiu):
     try:
@@ -15,35 +15,66 @@ def ComprovaArchiu(Arxiu):
 
 def init_dir():
     albums={}
-    for base, dirs, files in os.walk('/home/joel/Escritorio/python/2n/MP03/Pojecte-UF1/music'):
+    for base, dirs, files in os.walk(directori+"/music"):
         if files:
             song=[file for file in files if file.split(".")[1]=="mp3"]
             if ComprovaArchiu(base+"/info.txt"):
                 with open (base+"/info.txt","r") as f:
                     info=f.read()
                     albums[base]=album(base,song,info.split(":")[0],info.split(":")[2],info.split(":")[1],0)
-    with open ("/home/joel/Escritorio/python/2n/MP03/Pojecte-UF1/backups.txt","w") as f:
+    with open (directori+"/backups.txt","w") as f:
         f.writelines(" : ".join(['{0} : {1}'.format(key,albums[key].mostra()) for key in albums]))
+        
+def init():
+    albums={}
+    if ComprovaArchiu(directori+"/backups.txt"):
+        with open (directori+"/backups.txt","r") as f:
+            text=f.read()
+            list=text.split(":")
+            for i in range(len(list)):
+                if i%2!=0:
+                    info=list[i].split(",")
+                    albums[list[i-1]]=album(info[0],info[1].split("|"),info[2],info[3],info[4],info[5])
+    else:
+        albums={}
+    
+    if ComprovaArchiu(directori+"/estat_reproductor.txt"):
+        with open (directori+"/estat_reproductor.txt","r") as f:
+            info=f.readlines()
+            os.system("mpc add directori"+'/'+info[0])
+            os.system('mpc seek'+info[1].split("(")[1].replace(")",""))
+            os.system("mpc volume 30")
+
     return albums
 
-def init():
-    pass
+def Play_Pause():
+    os.system("mpc toggle")
+
+def Next():
+    os.system("mpc next")
+
+def Anterior():
+    os.system("mpc prev")
+
+def ValumeMas(num):
+    os.system("mpc volume + "+num)
+
+def ValumeMenos(num):
+    os.system("mpc volume - "+num)
+
 def reset():
-    directori="/home/joel/Escritorio/python/2n/MP03/Pojecte-UF1/music/"
-    os.system("rm -r "+directori)
+    os.system("rm -r "+directori+"/playlist/")
     init_dir()
     os.system("/etc/init.d/mpd restart")
     os.system("mpc update")
     init()
 
 def sortir():
-    directori="/home/joel/Escritorio/python/2n/MP03/Pojecte-UF1/"
-    stat=os.system("mpc status")
-    text=stat+' > '+directori+"estat_reproductor.txt"
+    text="mpc status"+" > "+directori+"estat_reproductor.txt"
     os.system(text)
 
-if __name__=="main":
-    init()
+if __name__=="__main__":
+    init_dir()
     menu=0
     while menu != 9:
         try:
