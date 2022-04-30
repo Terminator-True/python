daf3cd623c47b44bce50455c10fb35c482687437
Terminator_True <jf28renault@gmail.com>
2022-04-30 17:20:19 +0200

    080

--- 2n/MP03/Tasca6.1-Vistes/proves/app_principal/settings.py
+++ 2n/MP03/Tasca6.1-Vistes/proves/app_principal/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'vistes.apps.VistesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,7 +77,11 @@ WSGI_APPLICATION = 'proves.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'django',
+        'USER': 'admin_django',
+        'PASSWORD': 'Admin@123',
+        'HOST':"127.0.0.1",
+        'PORT':'3306'
     }
 }
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'GMT'
 
 USE_I18N = True
 
